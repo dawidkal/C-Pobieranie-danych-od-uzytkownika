57315121508c6f8714afe63aa9c9417dd0191139
Dawid <dawidkal@interia.pl>
2020-03-03 21:34:30 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2NF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )  // petla w nieskonczonosc
+            {
+                Console.WriteLine(" Podaj imie");
+                string name = Console.ReadLine();
+
+                Console.WriteLine(" Podaj wiek");
+                // wiek = int.Parse ( Console.ReadLine()); // konwersja na inta
+                int wiek;
+                bool rezultat = int.TryParse(Console.ReadLine(), out wiek);// zabespieczy jesli wprowadza niepoprawna wartosc ustawi wieik na 0
+                if (wiek > 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;  // zmiana koloru
+                    Console.WriteLine("Masz 18. Pijesz browara");
+                }
+                else if (rezultat == false)
+                {
+                    Console.WriteLine("Wprowadziles niepoprawna wartosc wiek");
+                }
+                else
+                {
+                    Console.WriteLine("Pijesz mleko");
+                }
+
+                Console.WriteLine("Wprowadziles wyraz -> " + name + " wiek " + wiek);
+                Console.ReadKey();
+                Console.Clear(); //czysci konsole
+                Console.ResetColor();
+            }
+        }
+
+    }
+}
